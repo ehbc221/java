ea012aa7852ec330e62792fef93187d1bb7e10b2
ehbc221 <ehbc221@gmail.com>
2017-06-05 03:03:38 +0000

    finished exercice 3 of toure/tp2

--- esp/2016-2017/toure/TP2/partie-b/exercice3/TestFraction.java
+++ esp/2016-2017/toure/TP2/partie-b/exercice3/TestFraction.java
@@ -3,12 +3,66 @@ import fraction.*;
 public class TestFraction {
 
     public static void main(String[] args) {
-        int f0 = new Fraction();
-        int f1 = new Fraction(6, 3);
-        int f0 = new Fraction(17);
-        int f0 = new Fraction("14/5");
+        // public Fraction ()
+        Fraction f0 = new Fraction();
+        // public String toString ()
+        System.out.println("Création de la fraction 0 : " + f0.toString());
+        // public Fraction (long, long)
+        Fraction f1 = new Fraction(6, 3);
+        // public String toString ()
+        System.out.println("Création de la fraction 1 : " + f1.toString());
+        // public Fraction (long)
+        Fraction f2 = new Fraction(17);
+        // public String toString ()
+        System.out.println("Création de la fraction 2 : " + f2.toString());
+        // public Fraction (String)
+        Fraction f3 = new Fraction("14/5");
+        // public String toString ()
+        System.out.println("Création de la fraction 3 : " + f3.toString());
+        // public void setNumerator (long num)
         f0.setNumerator(9);
+        // public void setDenominator (long denom)
         f0.setDenominator(2);
+        // public long numerator ()
+        System.out.println("Le numérateur de f3 est " + f3.numerator() + " et son dénominateur " + f3.denominator());
+        // public Fraction add (Fraction f)
+        f0 = f2.add(f3);
+        System.out.println("f0 après ajout de f2 à f3 : " + f0.toString());
+        // public Fraction substract (Fraction f)
+        f0 = f2.substract(f3);
+        System.out.println("f0 après soustraction de f3 à f2 : " + f0.toString());
+        // public Fraction multiply (Fraction f)
+        f0 = f2.multiply(f1);
+        System.out.println("f0 après multiplication de f2 à f1 : " + f0.toString());
+        // public Fraction divide (Fraction f)
+        f0 = f3.divide(f1);
+        System.out.println("f0 après division de f3 par f1 : " + f0.toString());
+        // public boolean equals
+        if (f0.equals(f3)) {
+            System.out.println("f3 : " + f3.toString() + " et f2 : " + f2.toString() + " sont égales.");
+        }
+        else {
+            System.out.println("f3 : " + f3.toString() + " et f2 : " + f2.toString() + " ne sont pas égales.");
+        }
+        Fraction f = new Fraction(28, 10);
+        // public boolean equals (Fraction f)
+        if (f.equals(f3)) {
+            System.out.println("f3 : " + f3.toString() + " et f : " + f.toString() + " sont égales.");
+        }
+        else {
+            System.out.println("f3 : " + f3.toString() + " et f : " + f.toString() + " ne sont pas égales.");
+        }
+        System.out.println("f3 : " + f3.toString());
+        // public int intValue()
+        System.out.println("\tSa valeur(en entier) est : " + f3.intValue());
+        // public long longValue()
+        System.out.println("\tSa valeur(en long) est : " + f3.longValue());
+        // public float floatValue()
+        System.out.println("\tSa valeur(en float) est : " + f3.floatValue());
+        // public double doubleValue()
+        System.out.println("\tSa valeur(en double) est : " + f3.doubleValue());
+        // public int hashCode()
+        System.out.println("Le hachcode de f3 : " + f.toString() + " est " + f3.hashCode());
     }
 
 }
--- esp/2016-2017/toure/TP2/partie-b/exercice3/fraction/Fraction.java
+++ esp/2016-2017/toure/TP2/partie-b/exercice3/fraction/Fraction.java
@@ -14,8 +14,8 @@ public class Fraction {
     */
 
     public Fraction() {
-        this.numerator = 0;
-        this.denominator = 0;
+        this.numerator = 1;
+        this.denominator = 1;
     }
 
     public Fraction(long n, long d) {
@@ -29,7 +29,7 @@ public class Fraction {
     }
 
     public Fraction(String ch) {
-
+        stringToFraction(ch);
     }
 
     /*
@@ -53,27 +53,39 @@ public class Fraction {
     }
 
     public Fraction add(Fraction f) {
-        this.numerator = (this.numerato * f.denominator) + (f.numerator * this.denominator);
-        this.denominator *= f.denominator;
+        Fraction f2 = new Fraction();
+        f2.numerator = (this.numerator * f.denominator) + (f.numerator * this.denominator);
+        f2.denominator = this.denominator * f.denominator;
+        f2.simplify();
+        return f2;
     }
 
-    public Fraction subtract(Fraction f) {
-        this.numerator = (this.numerato * f.denominator) - (f.numerator * this.denominator);
-        this.denominator *= f.denominator;
+    public Fraction substract(Fraction f) {
+        Fraction f2 = new Fraction();
+        f2.numerator = (this.numerator * f.denominator) - (f.numerator * this.denominator);
+        f2.denominator = this.denominator * f.denominator;
+        f2.simplify();
+        return f2;
     }
 
     public Fraction multiply(Fraction f) {
-        this.numerator *= f.numerator;
-        this.denominator *= f.denominator;
+        Fraction f2 = new Fraction();
+        f2.numerator = this.numerator * f.numerator;
+        f2.denominator = this.denominator * f.denominator;
+        f2.simplify();
+        return f2;
     }
 
     public Fraction divide(Fraction f) {
-        this.numerator *= f.denominator;
-        this.denominator *= f.numerator;
+        Fraction f2 = new Fraction();
+        f2.numerator = this.numerator * f.denominator;
+        f2.denominator = this.denominator * f.numerator;
+        f2 = f2.simplify();
+        return f2;
     }
 
     public boolean equals(Fraction f) {
-        if ((this.numerator * f.denominator) == (this.denominator *= f.numerator)) {
+        if ((this.numerator * f.denominator) == (this.denominator * f.numerator)) {
             return true;
         }
         else {
@@ -82,7 +94,11 @@ public class Fraction {
     }
 
     private void stringToFraction(String fString) {
-
+        String chaine;
+        chaine = fString.substring(0, fString.indexOf('/'));
+        this.numerator = Long.parseLong(chaine);
+        chaine = fString.substring(fString.indexOf('/') + 1, fString.length());
+        this.denominator = Long.parseLong(chaine);
     }
 
     public static long pgcd(long a, long b) {
@@ -91,6 +107,7 @@ public class Fraction {
 
     private Fraction simplify() {
         Fraction f = new Fraction(this.numerator, this.denominator);
+        long dividende;
         dividende = pgcd(f.numerator, f.denominator);
         f.numerator /= dividende;
         f.denominator /= dividende;
@@ -106,19 +123,19 @@ public class Fraction {
     }
 
     public float floatValue () {
-        return (float) (this.numerator / this.denominator);
+        return (float) (this.numerator) / this.denominator;
     }
 
     public double doubleValue () {
-        return (double) (this.numerator / this.denominator);
+        return (double) (this.numerator) / this.denominator;
     }
 
     public String toString () {
-        return "f = " + this.numertor + " / " + this.denominator;
+        return "f = " + this.numerator + " / " + this.denominator;
     }
 
     public int hashCode () {
-
+        return (int) (this.numerator / this.denominator);
     }
 
 }
